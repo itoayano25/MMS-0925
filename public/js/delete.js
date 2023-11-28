7b3b9edcfef424f6adc64c73a1ec1d2e0c721abf
itoayano25 <anoori1925@gmail.com>
2023-11-28 11:17:17 +0900

    非同期削除の部分修正

--- public/js/delete.js
+++ public/js/delete.js
@@ -1,23 +1,31 @@
 $(function() {
-    $(".delete-btn").click(function() {
+    $(".delete-btn").click(function(e) {
         var deleteConfirm = confirm('削除してよろしいですか？');
+        e.preventDefault()
 
         if(deleteConfirm == true){
             var clickEle = $(this)//thisはinputタグを参照する
             var productID = clickEle.attr('data-product_id');
-            console.log(productID);
-            
+
             $.ajaxSetup({
                 headers: {
                 "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),},
             });
+            
             //非同期通信
             $.ajax({
-                type: 'POST',
+                type: 'POST',//GETはパラメーターに表示される、POSTは表示されない
                 url: '/destroy/'+ productID,
-                dataType: 'json',
+                dataType: 'text', //dataTypeは返ってくる値のデータタイプ
                 data:{'id': productID},
             })
+            .done(function(){
+                //削除されるIDのtrを画面から削除をする⇒remove()
+                clickEle.parents('tr').remove();
+            })
+            .fail(function(){
+                alert('エラー')
+            });
         }else{
             (function(e) {
                 e.preventDefault()
