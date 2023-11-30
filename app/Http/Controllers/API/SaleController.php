4027881ab1d1d18c722d2f7485f0dd4906fb2d27
itoayano25 <anoori1925@gmail.com>
2023-11-30 18:32:20 +0900

    API実装

---
+++ app/Http/Controllers/API/SaleController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Sale;
+
+class SaleController extends Controller
+{
+    public function purchase(Request $request){
+        // リクエストから商品IDと購入数を取得
+        $productId = $request->input('product_id');
+        $quantity = $request->input('quantity', 1);
+
+        // DBから対象商品を検索・取得
+        $product = Product::find( $productId);
+
+        // 商品が存在していない、在庫不足の場合のバリデーション
+        if(!$product){
+            return response()->json(['message' => '商品が存在しません'],404);
+        }
+        if($product->stock < $quantity){
+            return response()->json(['message' => '商品が在庫不足です',400]);
+        }
+        // 在庫の減少
+        $product->stock -= $quantity;
+        $product->save();
+        // Saleテーブルに商品IDと購入日時を記録する
+        $sale = new Sale([
+            'product_id' => $productId,
+        ]);
+        $sale->save();
+
+        return response()->json(['message' => '購入成功']);
+    }
+}
--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -8,5 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Sale extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['product_id',];
     
+    
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
